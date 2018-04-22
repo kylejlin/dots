72bce2eacfa8e08c20d663998eb9eb123983c0d0
Kyle Lin <kylejlin.us@gmail.com>
2018-04-22 16:02:40 -0700

    Replace App with Dots

---
+++ src/Dots.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Dots.css'
+
+class Dots extends React.Component {
+  state = {
+    components: []
+  }
+
+  render() {
+    return (
+      <div className="Dots">
+
+      </div>
+    )
+  }
+}
+
+export default Dots
